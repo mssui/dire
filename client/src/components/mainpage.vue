4dab3c6b5f0797436a2f0c2ac3a90907ceb4a905
ASLI ARI <asliari2012@gmail.com>
2019-01-19 15:36:00 +0100

    Add fetch links Mlab

--- client/src/components/mainpage.vue
+++ client/src/components/mainpage.vue
@@ -2,12 +2,14 @@
   <div class="app">
     <navbar />
     
-   <div class="columns">
+   <div v-for="(entry,index) in loopEntries" :key="index" class="columns">
   <div class="column">
-    <p class="bd-notification is-info">First column</p>
+    
+          <strong>League Name :{{ entry.league_name }}</strong> <small>League Id:{{ entry.league_id}}</small>
+          
     <div class="columns is-mobile">
       <div class="column">
-        <p class="bd-notification is-info">First nested column</p>
+       <!-- First Card Box starts -->
 <div class="box">
   <article class="media">
     <div class="media-left">
@@ -24,31 +26,13 @@
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
         </p>
       </div>
-      <nav class="level is-mobile">
-        <div class="level-left">
-          <a class="level-item" aria-label="reply">
-            <span class="icon is-small">
-              <i class="fas fa-reply" aria-hidden="true"></i>
-            </span>
-          </a>
-          <a class="level-item" aria-label="retweet">
-            <span class="icon is-small">
-              <i class="fas fa-retweet" aria-hidden="true"></i>
-            </span>
-          </a>
-          <a class="level-item" aria-label="like">
-            <span class="icon is-small">
-              <i class="fas fa-heart" aria-hidden="true"></i>
-            </span>
-          </a>
-        </div>
-      </nav>
+    
     </div>
   </article>
 </div>
       </div>
       <div class="column">
-        <p class="bd-notification is-info">Second nested column</p>
+        <!-- Second Card Box starts -->
 <div class="box">
   <article class="media">
     <div class="media-left">
@@ -64,25 +48,7 @@
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
         </p>
       </div>
-      <nav class="level is-mobile">
-        <div class="level-left">
-          <a class="level-item" aria-label="reply">
-            <span class="icon is-small">
-              <i class="fas fa-reply" aria-hidden="true"></i>
-            </span>
-          </a>
-          <a class="level-item" aria-label="retweet">
-            <span class="icon is-small">
-              <i class="fas fa-retweet" aria-hidden="true"></i>
-            </span>
-          </a>
-          <a class="level-item" aria-label="like">
-            <span class="icon is-small">
-              <i class="fas fa-heart" aria-hidden="true"></i>
-            </span>
-          </a>
-        </div>
-      </nav>
+
     </div>
   </article>
 </div>
@@ -108,6 +74,7 @@ export default {
   data() {
     return {
       matches: [],
+      loopEntries: []
     };
   },
   components: {
@@ -119,12 +86,32 @@ export default {
   },
   
   created() {
-       axios.get('https://api.opendota.com/api/proMatches')
+       axios.get('/datas')
       .then(response => {
         this.matches = JSON.stringify(response.data);
         this.matches = JSON.parse(this.matches);
         console.log(this.matches);
-
+                 for (let i = 0; i < this.matches.length; i++) {
+          const currentData = this.matches[i];
+          this.loopEntries.push({
+            match_id: currentData.match_id,
+            duration: currentData.duration,
+            start_time: currentData.start_time,
+            radiant_team_id: currentData.radiant_team_id,
+            radiant_name: currentData.radiant_name,
+            dire_team_id: currentData.dire_team_id,
+            dire_dire_name: currentData.dire_dire_name,
+            leagueid: currentData.leagueid,
+            league_name: currentData.league_name,
+            series_id: currentData.series_id,
+            series_type: currentData.series_type,
+            radiant_score: currentData.radiant_score,
+            dire_score: currentData.dire_score,
+            radiant_win: currentData.radiant_win
+            });
+        }
+        console.log(this.loopEntries);
+        console.log(this.loopEntries[0]);
       })
       .catch(error => {
         console.log(error);
